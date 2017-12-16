e794daaa4af3892892e5c3cb9eef5a39f180a86b
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2017-12-16 14:48:20 +0300

    config changes and unused files are removed

--- app/config/Configuration.js
+++ app/config/Configuration.js
@@ -1,8 +1,8 @@
 import { Platform } from "react-native";
 
 const Configuration = {
-    API_URL : Platform.OS == "ios" ? "http://165.227.143.203:6060" : "http://165.227.143.203:6060",
-    STATIC_HOST : Platform.OS == "ios" ? "http://165.227.143.203:8800/" : "http://165.227.143.203:8800/",
+    API_URL : Platform.OS == "ios" ? "http://46.101.162.248:6060" : "http://46.101.162.248:6060",
+    STATIC_HOST : Platform.OS == "ios" ? "http://46.101.162.248:8800/" : "http://46.101.162.248:8800/",
     PLATFORM_IOS : Platform.OS == "ios",
     PLATFORM_ANDROID : Platform.OS != "ios",
     HTTP_TIMEOUT_MS : 40000 /* 40 sec */
--- index.ios.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native';
-import setup from './app/setup';
-
-AppRegistry.registerComponent('instanative', setup);
