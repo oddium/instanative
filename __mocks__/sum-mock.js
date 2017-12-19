cf36ef6f8d786e64284570b936ae5714e007a045
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2017-12-19 20:04:03 +0300

    module mocking examples are integrated

---
+++ __mocks__/sum-mock.js
@@ -0,0 +1,7 @@
+// test edilecek metod, normalde bir modül dosyasında
+// yer alabilir.
+const sum = (a, b) => {
+    return (a + b) * 2;
+}
+
+module.exports = sum;
---
+++ __tests__/sum-mock-test.js
@@ -0,0 +1,10 @@
+// jest ile bir modülü mocklamak için öncelikle mock 
+// modülünü yazarız ve jest'e mocklanacak modül yolunu
+// veririz ve require ile modüle erişiriz.
+jest.mock('sum-mock');
+
+test('adds 1 + 2 to equal 6', () => {
+    let sum = require('sum-mock');
+    console.log("sum ->", sum);
+    expect(sum(1, 2)).toBe(3);
+});
