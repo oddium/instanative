39b141c07e5a624be93e759157b0a226c2e3f4f3
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2017-12-22 06:28:10 +0300

    native toaster component integrated

--- android/app/src/main/java/com/instanative/MainApplication.java
+++ android/app/src/main/java/com/instanative/MainApplication.java
@@ -7,6 +7,7 @@ import com.dylanvann.fastimage.FastImageViewPackage;
 import com.vydia.RNUploader.UploaderReactPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.mol42.workshop.RNNativeToasterPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -30,7 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
             new FastImageViewPackage(),
             new UploaderReactPackage(),
             new RCTCameraPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNNativeToasterPackage()
       );
     }
 
